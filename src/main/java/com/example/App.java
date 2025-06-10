6f15d9b3fda411eb29912a029c8a8b2ceb6a249c
sanjana-rgb <sanjanag107@gmail.com>
2025-06-10 04:25:28 +0000

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("the sunm of 5 and 10 is 15");
+    }
+}
